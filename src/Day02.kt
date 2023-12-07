8e81218f64f4c4ae60ac6f70c7170db4c57b273f
Fatih Coşkun <fatih.coskun@doctari.de>
2023-12-07 08:52:11 +0100

    Day 02: Introduce Game class that parses a game input line.
    To get rid of duplicated code.
    Also use function expression body to further simplify code.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,61 +1,49 @@
 import Color.*
 
 enum class Color { red, green, blue }
+class QuantifiedColor(input: String) {
+  val quantity = input.split(" ")[0].toInt()
+  val color =  input.split(" ")[1].let { Color.valueOf(it) }
+}
 
-class GameConfig {
-  private val config  = mutableMapOf(
+class Game(input: String) {
+  val gameIndex = input.split(":")[0].split(" ")[1].toInt()
+  val draws = input.split(":")[1].split(";").map(::Draw)
+}
+
+class Draw(input: String) {
+  val quantifiedColors = input.split(",").map(String::trim).map(::QuantifiedColor)
+}
+
+class GameConfig(vararg config: Pair<Color, Int> = emptyArray()) {
+  private val config: MutableMap<Color, Int> = config.takeUnless { it.isEmpty() }?.let { mutableMapOf(*it) } ?: mutableMapOf(
     red to 0,
     green to 0,
     blue to 0,
   )
 
-  fun addMaxColorQuantity(color: Color, quantity: Int) {
-    if (config[color]!! < quantity) {
-      config[color] = quantity
+  fun addMaxColor(quantifiedColor: QuantifiedColor) {
+    if (config[quantifiedColor.color]!! < quantifiedColor.quantity) {
+      config[quantifiedColor.color] = quantifiedColor.quantity
     }
   }
-
-  fun addMaxColorQuantity(quantifiedColor: QuantifiedColor) = addMaxColorQuantity(quantifiedColor.color, quantifiedColor.quantity)
 
   fun hasMinQuantity(quantifiedColor: QuantifiedColor): Boolean = config[quantifiedColor.color]!! >= quantifiedColor.quantity
 
   fun toPower() = config[red]!! * config[green]!! * config[blue]!!
 }
 
-data class QuantifiedColor(val quantity: Int, val color: Color) {
-  companion object {}
-}
-
-fun QuantifiedColor.Companion.fromInput(input: String): QuantifiedColor {
-  val (quantity, color) = input.trim().split(" ")
-  return QuantifiedColor(quantity.toInt(), Color.valueOf(color))
-}
-
 fun main() {
-  fun part1(input: List<String>): Int {
-    val part1configuration = GameConfig().apply {
-      addMaxColorQuantity(red, 12)
-      addMaxColorQuantity(green, 13)
-      addMaxColorQuantity(blue, 14)
-    }
-
-    return input.sumOf { game ->
-      val (gameIndex, gameValue) = game.split(":")
-
-      gameIndex.split(" ")[1].toInt().takeIf {
-        gameValue.split(';').flatMap { draw -> draw.split(",").map { part1configuration.hasMinQuantity(QuantifiedColor.fromInput(it)) } }.all { it }
-      } ?: 0
-    }
+  fun part1(input: List<String>) = GameConfig(red to 12, green to 13, blue to 14).let { config ->
+    input.map(::Game).filter{ game ->
+      game.draws.flatMap { draw -> draw.quantifiedColors.map { config.hasMinQuantity(it) } }.all { it }
+    }.sumOf { it.gameIndex }
   }
 
-  fun part2(input: List<String>) = input.sumOf { game ->
-    val gameConfig = GameConfig()
-
-    game.split(":")[1].split(';').flatMap { draw -> draw.split(",") }.forEach {
-      gameConfig.addMaxColorQuantity(QuantifiedColor.fromInput(it))
-    }
-
-    gameConfig.toPower()
+  fun part2(input: List<String>) = input.map(::Game).sumOf { game ->
+    game.draws.flatMap { draw -> draw.quantifiedColors }.fold(GameConfig()) { config, quantifiedColor ->
+      config.apply { addMaxColor(quantifiedColor) }
+    }.toPower()
   }
 
   // test if implementation meets criteria from the description, like:
